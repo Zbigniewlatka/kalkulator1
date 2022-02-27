3f8ccdcef19c10b873ab17117ad6869aa5aa67e3
Zbigniewlatka <zbychl@gmail.com>
2022-02-27 10:34:30 +0100

    dodałem plik kalk2.nim z funkcja case do konfiguracji

--- kalk2.nim
+++ kalk2.nim
@@ -2,6 +2,9 @@ import kalk1
 import strutils
 import math
 
+echo "Podaj działanie : (+),(-),( /), (*) : "
+let x = readline(stdin).parseFloat()
+
 echo "Podaj liczbę "
 let g = readline(stdin).parseFloat()
 
@@ -15,3 +18,15 @@ echo minus(g, b)
 echo divd(g, b).round(2)
 echo multip(g, b)
 
+
+#case x
+#of +:
+#  echo "Five!"
+#of -:
+#  echo "Seven!"
+#of /:
+#  echo "Ten!"
+#of *:
+#  echo "Ten!"
+#else:
+#  echo "zły wybór spróbuje jeszcze raz"
