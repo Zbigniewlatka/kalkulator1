c783064512dcb88d01c98b8add263126c4890195
Zbigniewlatka <zbychl@gmail.com>
2022-03-01 12:53:55 +0100

    zmiana znaczników operacji na cyfry

--- kalk2.nim
+++ kalk2.nim
@@ -1,10 +1,12 @@
-iimport kalk1
+import kalk1
 import strutils
 import math
-import parseChar from std/strutils
 
-echo "Podaj działanie : (+),(-),(/),(*) : "
-let i = readline(stdin).parseChar()
+
+
+echo "Podaj symbol :  dodawanie-1, różnica-2, iloraz-3, iloczyn-4 : "
+let ch = readline(stdin).parseInt()
+
 
 echo "Podaj liczbę "
 let g = readline(stdin).parseFloat()
@@ -15,14 +17,18 @@ let b = readline(stdin).parseFloat()
 echo "=====================\n"
 echo " Wyniki działań"
 
-case i
-of '+':
+case ch
+of 1:
+  echo "Wynik dodawania to :"
   echo  plus(g, b)
-of '-':
+of 2:
+  echo "Wynik odejmowania to :"
   echo minus(g, b)
-of '/':
+of 3:
+  echo "Wynik dzielenia to :"
   echo divd(g, b).round(2)
-of '*':
+of 4:
+  echo "Wynik mnożenia  to :"
   echo multip(g, b)
 else:
   echo "zły wybór spróbuje jeszcze raz"
